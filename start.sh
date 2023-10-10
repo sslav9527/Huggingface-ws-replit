6c650b142f2a0e1b30bddfb32b9fc25e1c2b6dd2
sslav9527 <97280575+sslav9527@users.noreply.github.com>
2023-10-10 21:51:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 export NEZHA_SERVER="nezha.sslav.eu.org:5555"
-export NEZHA_KEY="gjTIshHnoDK2BCz52d"
+export NEZHA_KEY="VOdTnaAMD0ZOHzymFL"
 
 chmod +x server start.sh
 nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #！若需要tls，在此句 > 前面加上--tls即可
